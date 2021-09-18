6d81835ffe69efeaa55587ba96b085ea295a9b76
MD.Mahmodul Hassan <55808463+mahmodulhassan37@users.noreply.github.com>
2021-09-18 11:25:13 +0600

    Add files via upload

---
+++ DataType.py
@@ -0,0 +1,60 @@
+#floate number
+
+##x = 1.10
+##y = 1.0
+##z = -35.59
+##
+##print (type(x))
+##print(type(y))
+##print(type(z))
+
+
+##x= 35e3       #e denoted by power
+##y = 12E4
+##z=-87.7e100
+##
+##print(type(x))
+##print(type(y))
+##print(type(z))
+
+
+#complex number
+
+##x=3+5j    # j is denoted imaginary part
+##y = 5j
+##z = -5j
+##
+##print(type(x))
+##print(type(y))
+##print(type(z))
+
+
+
+#type conversion
+
+##x = 2
+##y = 3.5
+##z =1j
+##
+###convert from int to floate
+##a = float(x)
+##
+###convert from float to int
+##b=int(y)
+##
+###convert from int to complex
+##c=complex(x)
+##
+##print(a)
+##print(b)
+##print(c)
+##
+##
+##print(type(a))
+##print(type(b))
+##print(type(c))
+
+##
+### built in random function can used to make random number
+##import random
+##print(random.randrange(1,10))
